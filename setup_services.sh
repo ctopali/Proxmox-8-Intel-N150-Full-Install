a8c659ac9aa3fab5fa4efd3a1d4f8c1cbb0829d9
AlexDrakul <36307353+ctopali@users.noreply.github.com>
2026-07-13 14:42:01 -0400

    Update setup_services.sh

--- setup_services.sh
+++ setup_services.sh
@@ -1,1 +1,10 @@
-# source "$(dirname "$0")/../infra.conf"
+#creamos el directorio de configuracion de los helper script si no existe
+mkdir -p /usr/local/community-scripts/defaults
+
+echo "--- 1. Incorporamos la infrastructura de redes: ---"
+wget -O /scripts/infra.conf "https://raw.githubusercontent.com/ctopali/Proxmox-8-Intel-N150-Full-Install/refs/heads/main/infra.conf"
+source /scripts/infra.conf
+
+echo "--- 2. Copiamos los archvios .vars para la instalacion Helper-Scripts: ---"
+wget -O /usr/local/community-script/defaults/adguard_test.vars "https://raw.githubusercontent.com/ctopali/Proxmox-8-Intel-N150-Full-Install/refs/heads/main/configs/adguard_test.vars"
+cat /usr/local/community-script/defaults/adguard_test.vars
