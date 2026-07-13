dc84efb7bcfd2ac2a2c6639f8cf1cdc9d6735f3c
AlexDrakul <36307353+ctopali@users.noreply.github.com>
2026-07-13 14:53:21 -0400

    Update setup_services.sh

--- setup_services.sh
+++ setup_services.sh
@@ -1,10 +1,29 @@
+#!/usr/bin/env bash
+set -e
+
 #creamos el directorio de configuracion de los helper script si no existe
 mkdir -p /usr/local/community-scripts/defaults
 
-echo "--- 1. Incorporamos la infrastructura de redes: ---"
-wget -O /scripts/infra.conf "https://raw.githubusercontent.com/ctopali/Proxmox-8-Intel-N150-Full-Install/refs/heads/main/infra.conf"
+echo "--- 1. Descargando infraestructura de red ---"
+
+curl -fsSL \
+"https://raw.githubusercontent.com/ctopali/Proxmox-8-Intel-N150-Full-Install/refs/heads/main/infra.conf" \
+-o /scripts/infra.conf
+
 source /scripts/infra.conf
 
-echo "--- 2. Copiamos los archvios .vars para la instalacion Helper-Scripts: ---"
-wget -O /usr/local/community-script/defaults/adguard_test.vars "https://raw.githubusercontent.com/ctopali/Proxmox-8-Intel-N150-Full-Install/refs/heads/main/configs/adguard_test.vars"
-cat /usr/local/community-script/defaults/adguard_test.vars
+echo "Infraestructura cargada."
+echo "Gateway : $GATEWAY"
+echo "DNS     : $LOCAL_DNS"
+
+echo "--- 2. Descargando archivo de configuración del Helper Script ---"
+
+curl -fsSL \
+"https://raw.githubusercontent.com/ctopali/Proxmox-8-Intel-N150-Full-Install/refs/heads/main/configs/adguard_test.vars" \
+-o /usr/local/community-scripts/defaults/adguard_test.vars
+
+echo
+echo "Contenido de adguard_test.vars:"
+echo "--------------------------------"
+
+cat /usr/local/community-scripts/defaults/adguard_test.vars
