110eae4cde693e5fb266512950685e2929727934
AlexDrakul <36307353+ctopali@users.noreply.github.com>
2026-07-20 14:18:42 -0400

    Update setup_full.sh

--- setup_full.sh
+++ setup_full.sh
@@ -1,19 +1,55 @@
 #!/usr/bin/env bash
 set -e
 
-echo "1. Copiamos los scripts que se utilizaran:"
+inst-script() {
+    local SCRIPT="$1"
+    local PREGUNTA="$2"
+
+    echo
+    read -rp "$PREGUNTA [y/N]: " RESP
+
+    case "${RESP,,}" in
+        y|yes|s|si|sí)
+            echo "Continuando..."
+
+            if [[ -f "/scripts/$SCRIPT" ]]; then
+                echo "$SCRIPT ya está descargado."
+            else
+                echo "Descargando $SCRIPT..."
+                curl -fsSL \
+                    "https://raw.githubusercontent.com/ctopali/Proxmox-8-Intel-N150-Full-Install/refs/heads/main/$SCRIPT" \
+                    -o "/scripts/$SCRIPT"
+            fi
+
+            echo "Ejecutando $SCRIPT..."
+            bash "/scripts/$SCRIPT"
+            ;;
+
+        *)
+            echo "Se omitió $SCRIPT."
+            return 0
+            ;;
+    esac
+}
+
+echo "1. Instalar sensores y drivers de la MB IT87 (si aplica):"
+inst-script \
+    "setup_sensors.sh" \
+    "¿Desea instalar los controladores y sensores de la Tarjeta Madre IT87?"
+
+echo "2. Instalar Home Assistant (VM):"
+inst-script \
+    "setup_sensors.sh" \
+    "¿Desea instalar los controladores y sensores de la Tarjeta Madre IT87?"
 
 curl -fsSL \
-"https://raw.githubusercontent.com/ctopali/Proxmox-8-Intel-N150-Full-Install/refs/heads/main/setup_sensors.sh" \
--o /scripts/setup_sensors.sh
-echo "1.1. Script de sensors... CHECK"
-
-curl -fsSL \
-"https://raw.githubusercontent.com/ctopali/Proxmox-8-Intel-N150-Full-Install/refs/heads/main/setup_services.sh" \
--o /scripts/setup_services.sh
-echo "1.2. Script de services... CHECK"
+"https://raw.githubusercontent.com/ctopali/Proxmox-8-Intel-N150-Full-Install/refs/heads/main/setup_startup.sh" \
+-o /scripts/setup_startup.sh
+echo "1.3. Script de setup_startup... CHECK"
 
 echo "2. Ejecucuion de Scripts y comandos bash:"
 echo "2.1 Ejecutando setup_sesors.sh:"
 bash /scripts/setup_sensors.sh
+
+echo "2.2 Ejecutando setup_services.sh:"
 bash /scripts/setup_services.sh
