c5842a86a061e191f0ad66439158e5029b388fd6
AlexDrakul <36307353+ctopali@users.noreply.github.com>
2026-07-20 16:44:32 -0400

    Update install_adguard.sh

--- install_adguard.sh
+++ install_adguard.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
+source /scripts/infra.conf
+source /scripts/lib.sh
+
 echo "Creando archivo vars: ---"
 #           APP       HOSTNAME  IP           CPU RAM DISK TUN GPU NEST
 create_vars "AdGuard" "adguard" "debian" "13" "$ADGUARD_IP" 1 512 2 yes no 0
