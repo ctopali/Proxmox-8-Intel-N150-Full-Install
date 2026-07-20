af2e2ae4aad45c2e50c27b705a6f91bc731f3674
AlexDrakul <36307353+ctopali@users.noreply.github.com>
2026-07-20 15:24:49 -0400

    Update install_frigate.sh

--- install_frigate.sh
+++ install_frigate.sh
@@ -3,3 +3,5 @@ create_vars "Frigate" "frigate" "debian" "13" "$FRIGATE_IP" 4 4096 32 yes yes 1
 
 echo "Instalación del servicio Frigate:"
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/ct/frigate.sh)"
+
+# Modificación del archivo yaml de configuración de Frigate:
