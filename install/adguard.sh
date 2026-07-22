d1641e09206747e4e81311b0ca4f68d2f8bc9003
AlexDrakul <36307353+ctopali@users.noreply.github.com>
2026-07-22 12:46:27 -0400

    Update adguard.sh

--- install/adguard.sh
+++ install/adguard.sh
@@ -7,3 +7,6 @@ source /scripts/lib.sh
 echo "Creando archivo vars: ---"
 #           APP       HOSTNAME  IP           CPU RAM DISK TUN GPU NEST
 create_vars "AdGuard" "adguard" "debian" "13" "$ADGUARD_IP" 1 512 2 yes no 0
+
+# Ejecutamos el Helper-Script:
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/ct/adguard.sh)"
