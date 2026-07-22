2a1ad9b62a746e0121e10ab2ff1df2fec8611e21
AlexDrakul <36307353+ctopali@users.noreply.github.com>
2026-07-22 13:00:20 -0400

    Update adguard.sh

--- install/adguard.sh
+++ install/adguard.sh
@@ -10,7 +10,7 @@ create_vars "AdGuard" "adguard" "debian" "13" "$ADGUARD_IP" 1 512 2 yes no 0
 
 echo "A continuación se ejecutará la instalación Helper-Script de"
 echo "Adguard Home, por favor, selecciona no compartir user data"
-echo "y también selecciona en template 'User Default'."
+echo "y también selecciona en template 'App Defaults ...'."
 echo
 echo "Ejecutando Instalación de Adguard Home: ---"
 read -rp "Press Enter to continue..."
