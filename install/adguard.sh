efbade36746f7d7474bf5509834507ecb39f2573
AlexDrakul <36307353+ctopali@users.noreply.github.com>
2026-07-22 12:55:57 -0400

    Update adguard.sh

--- install/adguard.sh
+++ install/adguard.sh
@@ -8,5 +8,10 @@ echo "Creando archivo vars: ---"
 #           APP       HOSTNAME  IP           CPU RAM DISK TUN GPU NEST
 create_vars "AdGuard" "adguard" "debian" "13" "$ADGUARD_IP" 1 512 2 yes no 0
 
+echo "A continuación se ejecutará la instalación Helper-Script de"
+echo "Adguard Home, por favor, selecciona no compartir user data"
+echo "y también selecciona en template 'User Default'."
+echo
 echo "Ejecutando Instalación de Adguard Home: ---"
+read -rp "Press Enter to continue..."
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/ct/adguard.sh)"
