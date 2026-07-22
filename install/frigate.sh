35beae853a33f27a914d98a3cbf040100c5a4cfb
AlexDrakul <36307353+ctopali@users.noreply.github.com>
2026-07-22 19:08:37 -0400

    Update frigate.sh

--- install/frigate.sh
+++ install/frigate.sh
@@ -27,5 +27,66 @@ if ! touch /media/frigate/recordings/prueba then
   
 # Modificación del archivo yaml de configuración de Frigate:
 cat pct $CTID config.yaml < EOF
-
+mqtt:
+  enabled: false
+ffmpeg:
+  hwaccel_args: auto
+detectors:
+  detector01:
+    type: openvino
+    device: AUTO
+detect:
+  enabled: true
+record:
+  enabled: false
+objects:
+  track:
+    - person
+    - car
+    - dog
+    - cat
+go2rtc:
+  streams:
+    exterior_main:
+      - "rtsp://admin:123456@192.168.1.30:554/stream1" # High quality
+    exterior_sub:
+      - "rtsp://admin:123456@192.168.1.30:554/stream2" # Low quality
+    patio1_main:
+      - "rtsp://admin:123456@192.168.1.31:554/stream1" # High quality
+    patio1_sub:
+      - "rtsp://admin:123456@192.168.1.31:554/stream2" # Low quality
+    patio2_main:
+      - "rtsp://admin:123456@192.168.1.32:554/stream1" # High quality
+    patio2_sub:
+      - "rtsp://admin:123456@192.168.1.32:554/stream2" # Low quality
+cameras:
+  exterior:
+    enabled: true
+    ffmpeg:
+      inputs:
+        - path: rtsp://127.0.0.1:8554/exterior_sub
+          roles:
+            - detect
+  patio1:
+    enabled: true
+    ffmpeg:
+      inputs:
+        - path: rtsp://127.0.0.1:8554/patio1_sub
+          roles:
+            - detect
+  patio2:
+    enabled: true
+    ffmpeg:
+      inputs:
+        - path: rtsp://127.0.0.1:8554/patio2_sub
+          roles:
+            - detect
+model:
+  width: 300
+  height: 300
+  input_tensor: nhwc
+  input_pixel_format: bgr
+  path: /openvino-model/ssdlite_mobilenet_v2.xml
+  labelmap_path: /openvino-model/coco_91cl_bkgr.txt
+version: 0.17-0
 EOF
