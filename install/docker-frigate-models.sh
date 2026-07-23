48ebf007dcaafb927a0f30bdb683d20feae55e79
AlexDrakul <36307353+ctopali@users.noreply.github.com>
2026-07-23 10:31:47 -0400

    Update docker-frigate-models.sh

--- install/docker-frigate-models.sh
+++ install/docker-frigate-models.sh
@@ -1,4 +1,20 @@
+#!/usr/bin/env bash
+set -e
 
+source /scripts/infra.conf
+source /scripts/lib.sh
+
+echo "Creando archivo vars: ---"
+#           APP       HOSTNAME  IP           CPU RAM DISK TUN GPU NEST
+create_vars "docker-frigate-models" "docker-frigate-models" "debian" "13" "$DOCKER_IP" 1 512 20 yes no 0
+
+echo "A continuación se ejecutará la instalación Helper-Script de"
+echo "Docker, por favor, selecciona no compartir user data"
+echo "y también selecciona en template 'App Defaults ...'."
+echo
+echo "Ejecutando Instalación de Docker: ---"
+read -rp "Press Enter to continue..."
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/ct/docker.sh)"
 
 
 # App-specific defaults for Docker (docker)
