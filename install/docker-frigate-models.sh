60442a114ecf3c7f63c8f95cd8ed67da5fbfaf31
AlexDrakul <36307353+ctopali@users.noreply.github.com>
2026-07-23 10:23:42 -0400

    Update docker-frigate-models.sh

--- install/docker-frigate-models.sh
+++ install/docker-frigate-models.sh
@@ -1,1 +1,31 @@
 
+
+
+# App-specific defaults for Docker (docker)
+# Generated on 2026-07-23T13:15:14Z
+
+var_os=debian
+var_version=13
+var_unprivileged=1
+var_cpu=2
+var_ram=2048
+var_disk=6
+var_brg=vmbr0
+var_net=192.168.1.19/24
+var_gateway=192.168.1.254
+var_ipv6_method=auto
+var_ssh=no
+var_apt_cacher=no
+var_fuse=no
+var_tun=no
+var_gpu=no
+var_nesting=1
+var_keyctl=1
+var_mknod=0
+var_protection=no
+var_timezone=America/Santiago
+var_tags=
+var_verbose=yes
+var_hostname=docker-frigate-models
+var_template_storage=local
+var_container_storage=local-lvm
